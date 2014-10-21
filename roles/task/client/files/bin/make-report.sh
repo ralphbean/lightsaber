14ef3093bfccfab7a9d9ae58079ce74c32286a36
Ralph Bean <rbean@redhat.com>
2014-10-21 09:41:15 -0400

    Stuff needed for making reports.

--- roles/task/client/files/bin/make-report.sh
+++ roles/task/client/files/bin/make-report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 today=$(date +%Y-%m-%d)
-/home/threebean/bin/timesheet.sh | ansi2html --linkify > /tmp/timesheet.html
+/usr/local/bin/timesheet.sh | ansi2html --linkify > /tmp/timesheet.html
 scp /tmp/timesheet.html threebean.org:~/webapps/static/timesheets/$today.html
 scp /tmp/timesheet.html threebean.org:~/webapps/static/timesheets/latest.html
 rm /tmp/timesheet.html
