b02343359c0faf7903bc710de59d95cf4d10f405
Ralph Bean <rbean@redhat.com>
2015-02-09 09:22:15 -0500

    Update to task theming.

--- roles/task/client/files/bin/timesheet.sh
+++ roles/task/client/files/bin/timesheet.sh
@@ -7,26 +7,27 @@ fmt="%Y-%m-%d"
 start=$(date +$fmt -d $phrase)
 end=$(date +$fmt)
 filter="project.isnt:apply project.isnt:family project.isnt:xmas project.isnt:cersc project.isnt:iso project.isnt:monroe project.isnt:house project.isnt:misc project.isnt:rit project.isnt:tos-rit-projects-seminar project.isnt:music project.isnt:hfoss project.isnt:wrns"
+config="rc.defaultwidth=190 rc.defaultheight=75 rc._forcecolor=yes"
 
 echo "    (generated at $(date))"
 echo
 echo " -- Tasks completed from $start to $end (back $phrase) -- "
-/usr/bin/task $filter end.after:$start work_report
+/usr/bin/task $config $filter end.after:$start work_report
 
 echo
 echo 
 echo " -- Upcoming tasks -- "
-/usr/bin/task $filter next
+/usr/bin/task $config $filter next
 
 echo
 echo
 echo " -- Summary -- "
-/usr/bin/task $filter summary
+/usr/bin/task $config $filter summary
 
 echo
 echo
 echo " -- History -- "
-/usr/bin/task $filter history
-/usr/bin/task $filter ghistory
-/usr/bin/task burndown.daily
-/usr/bin/task burndown
+/usr/bin/task $config $filter history
+/usr/bin/task $config $filter ghistory
+/usr/bin/task $config burndown
+/usr/bin/task $config burndown.daily
