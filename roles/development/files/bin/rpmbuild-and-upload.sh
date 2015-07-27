3739d04c27b361e91eb8134721ba26d9e38a599e
Ralph Bean <rbean@redhat.com>
2015-07-27 11:27:27 -0400

    Buckets -> duckets.

--- roles/development/files/bin/rpmbuild-and-upload.sh
+++ roles/development/files/bin/rpmbuild-and-upload.sh
@@ -5,8 +5,12 @@ spec=$1
 srpm=$(rpmbuild -bs $spec | awk ' { print $2 } ')
 srpm=$(python -c "import os.path; print os.path.relpath('$srpm')")
 
-scp $spec threebean.org:~/webapps/static/rpm/SPECS/.
-scp $srpm threebean.org:~/webapps/static/rpm/SRPMS/.
+cp $spec ~/scratch/threebean.org/rpm/SPECS/.
+cp $srpm ~/scratch/threebean.org/rpm/SRPMS/.
+
+# Buckets -> Duckets
+/home/threebean/.virtualenvs/awscli/bin/aws s3 sync \
+    ~/scratch/threebean.org/ s3://threebean.org
 
 echo "Spec URL: http://threebean.org/rpm/$spec"
 echo "SRPM URL: http://threebean.org/rpm/$srpm"
